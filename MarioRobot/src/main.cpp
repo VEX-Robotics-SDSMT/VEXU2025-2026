0db901e1773483a087490243e1e7b9e29d04b72c
connoraxtell-mines <connor.axtell@mines.sdsmt.edu>
2026-01-13 22:35:56 -0700

    tested linking for new library

--- MarioRobot/src/main.cpp
+++ MarioRobot/src/main.cpp
@@ -1,6 +1,15 @@
 #include "main.h"
 #include "PID.h"
+#include "test.h"
 
+pros::MotorGroup left_motors({-3,-2,-1,4}, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::MotorGroup right_motors({5,7,8,-6}, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+
+pros::MotorGroup topIntakeMotors({-19,20}, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor sorterMotor(12, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor botomIntakeMotor(-11, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor middleIntakeMotor(13, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
+pros::Motor basketOuttakeMotor(18, pros::v5::MotorGears::blue, pros::v5::MotorUnits::degrees);
 
 void initialize() {}
 
@@ -13,25 +22,21 @@ void autonomous() {}
 
 void opcontrol() 
 {
+	testFunction();
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 
-	pros::Motor motor(1, pros::MotorGears::blue, pros::MotorEncoderUnits::degrees);
 
-	Mines::PIDTuning tuning;
-	tuning.kP = 1.0;
-	tuning.kI = 0.5;
-	tuning.kD = 0;
-
-	tuning.integralClamp = 100.0;
-	tuning.maxOutput = 127.0;
-	tuning.minOutput = -127.0;
-
-	Mines::PID pid(tuning);
-
-	
+	topIntakeMotors.move(-127);
+	//botomIntakeMotor.move(127);
+	middleIntakeMotor.move(-127);
+	basketOuttakeMotor.move(127);
+	//sorterMotor.move(127);
 
 	while (true) 
 	{
+		left_motors.move(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+		right_motors.move(master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y));
+
 		pros::delay(20);
 	}
 }
