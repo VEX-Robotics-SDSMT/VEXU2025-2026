753b500716914d7e19b7b9682444b91a51303c76
bentonchristiansen <benton.christiansen@mines.sdsmt.edu>
2026-02-28 16:05:40 -0700

    changed

--- SuperMario/src/main.cpp
+++ SuperMario/src/main.cpp
@@ -61,11 +61,13 @@ void competition_initialize() {}
  * task, not resume it from where it left off.
  */
 void opcontrol() {
+	int target;
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::Motor motor(1,pros::v5::MotorGears::green,pros::v5::MotorEncoderUnits::degrees);
 
-	Mines::PID pid(90, .001, .5, .0005);
-	pid.setTarget(5100);
+	Mines::PID pid(5, .001, .5, .5);
+	target = 5100;
+	pid.setTarget(target);
 
 	while(true)
 	{
