5be044385b1756b695fc8ae2b4caab9d227d23ba
connoraxtell-mines <connor.axtell@mines.sdsmt.edu>
2026-02-28 15:11:59 -0700

    changed

--- SuperMario/src/main.cpp
+++ SuperMario/src/main.cpp
@@ -62,20 +62,17 @@ void competition_initialize() {}
  */
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
+	pros::Motor motor(1,pros::v5::MotorGears::green,pros::v5::MotorEncoderUnits::degrees);
 
+	Mines::PID pid(10, .001, .5, .5);
+	pid.setTarget(5100);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+	while(true)
+	{
+		pid.calculate(motor.get_position());
+		pid.reset();
+		motor.move(pid.calculate(motor.get_position()));
+		pros::delay(10);
 	}
+
 }
