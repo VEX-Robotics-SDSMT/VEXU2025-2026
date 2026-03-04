1463f90023f9900c2544e52d83336c271635a1ad
Andrew Bahm <andrew.bahm@mines.sdsmt.edu>
2026-03-03 22:21:10 -0700

    Yoshi Auto
    Yoshi Match Atuo nearing completion

---
+++ MarioRobot/.d/PID.cpp.d
@@ -0,0 +1,5 @@
+bin/PID.cpp.o: src/PID.cpp include/PID.h vendor/pros/rtos.h \
+ vendor/pros/rtos.hpp
+include/PID.h:
+vendor/pros/rtos.h:
+vendor/pros/rtos.hpp:
---
+++ MarioRobot/.d/main.cpp.d
@@ -0,0 +1,57 @@
+bin/main.cpp.o: src/main.cpp include/main.h vendor/pros/api.h \
+ vendor/pros/adi.h vendor/pros/ai_vision.h vendor/pros/colors.h \
+ vendor/pros/device.h vendor/pros/distance.h vendor/pros/error.h \
+ vendor/pros/ext_adi.h vendor/pros/adi.h vendor/pros/gps.h \
+ vendor/pros/imu.h vendor/pros/link.h vendor/pros/llemu.h \
+ vendor/pros/misc.h vendor/pros/motors.h vendor/pros/optical.h \
+ vendor/pros/error.h vendor/pros/rotation.h vendor/pros/rtos.h \
+ vendor/pros/screen.h vendor/pros/vision.h vendor/pros/adi.hpp \
+ vendor/pros/ai_vision.hpp vendor/pros/device.hpp vendor/pros/misc.hpp \
+ vendor/pros/rtos.hpp vendor/pros/colors.hpp vendor/pros/distance.hpp \
+ vendor/pros/gps.hpp vendor/pros/imu.hpp vendor/pros/link.hpp \
+ vendor/pros/llemu.hpp vendor/pros/motor_group.hpp \
+ vendor/pros/abstract_motor.hpp vendor/pros/rtos.hpp \
+ vendor/pros/motors.hpp vendor/pros/optical.hpp vendor/pros/rotation.hpp \
+ vendor/pros/screen.hpp vendor/pros/vision.hpp include/PID.h
+include/main.h:
+vendor/pros/api.h:
+vendor/pros/adi.h:
+vendor/pros/ai_vision.h:
+vendor/pros/colors.h:
+vendor/pros/device.h:
+vendor/pros/distance.h:
+vendor/pros/error.h:
+vendor/pros/ext_adi.h:
+vendor/pros/adi.h:
+vendor/pros/gps.h:
+vendor/pros/imu.h:
+vendor/pros/link.h:
+vendor/pros/llemu.h:
+vendor/pros/misc.h:
+vendor/pros/motors.h:
+vendor/pros/optical.h:
+vendor/pros/error.h:
+vendor/pros/rotation.h:
+vendor/pros/rtos.h:
+vendor/pros/screen.h:
+vendor/pros/vision.h:
+vendor/pros/adi.hpp:
+vendor/pros/ai_vision.hpp:
+vendor/pros/device.hpp:
+vendor/pros/misc.hpp:
+vendor/pros/rtos.hpp:
+vendor/pros/colors.hpp:
+vendor/pros/distance.hpp:
+vendor/pros/gps.hpp:
+vendor/pros/imu.hpp:
+vendor/pros/link.hpp:
+vendor/pros/llemu.hpp:
+vendor/pros/motor_group.hpp:
+vendor/pros/abstract_motor.hpp:
+vendor/pros/rtos.hpp:
+vendor/pros/motors.hpp:
+vendor/pros/optical.hpp:
+vendor/pros/rotation.hpp:
+vendor/pros/screen.hpp:
+vendor/pros/vision.hpp:
+include/PID.h:
