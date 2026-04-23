a5d768059cbe47643b5bbbbd2438792144fe5a15
bentonchristiansen <benton.christiansen@mines.sdsmt.edu>
2026-04-23 09:40:50 -0600

    Updated ports
    Updated ports with mario rebuild.

--- SuperMario/include/devices.h
+++ SuperMario/include/devices.h
@@ -11,26 +11,30 @@ inline pros::Controller master(pros::controller_id_e_t::E_CONTROLLER_MASTER);
 //===============================================
 
 //Left Motors
-const inline int8_t LM1 = 12;
-const inline int8_t LM2 = 7;
-const inline int8_t LM3 = 2;
+const inline int8_t LM1 = 4;
+const inline int8_t LM2 = 1;
+const inline int8_t LM3 = 5;
 const inline int8_t LM4 = 3;
 
 //Right Motors
-const inline int8_t RM1 = 6;
-const inline int8_t RM2 = 10;
-const inline int8_t RM3 = 1;
-const inline int8_t RM4 = 9;
+const inline int8_t RM1 = 9;
+const inline int8_t RM2 = 7;
+const inline int8_t RM3 = 6;
+const inline int8_t RM4 = 18;
 
 //Intake Motors
-const inline int8_t IN1 = 20;
-const inline int8_t IN2 = 5;
+const inline int8_t IN1 = 19;
+const inline int8_t IN2 = 10;
 
 //Encoder
-const inline int8_t ENC = 4;
+const inline int8_t ENC = 14;
 
 //Inertial
-const inline uint8_t INERT = 16;
+const inline uint8_t INERT = 20;
+
+//De-Loader Arm
+const inline int8_t UL1 = 2;
+const inline int8_t UL2 = 17;
 
 //Pneumatics
 const inline char HOOD_PORT = 'F';
@@ -48,6 +52,9 @@ inline pros::MotorGroup rightMotors({RM1, RM2, RM3, RM4});
 
 //Intake Group
 inline pros::MotorGroup intakeMotors({IN1, -IN2});
+
+//Loader Arms
+inline pros::MotorGroup unLoader({UL1, -UL2});
 
 //===============================================
 //Sensors
--- SuperMario/src/opcontrol.cpp
+++ SuperMario/src/opcontrol.cpp
@@ -20,6 +20,14 @@ void leftArcadeDrive(pros::v5::Controller& controller, const pros::v5::AbstractM
 	right.move(drivePower -  turnPower);
 }
 
+void leftArcadeDriveSwapped(pros::v5::Controller& controller, const pros::v5::AbstractMotor& left, const pros::v5::AbstractMotor& right, pros::controller_analog_e_t axis1, pros::controller_analog_e_t axis2)
+{
+	int32_t drivePower = mapExponential(controller.get_analog(axis1));
+	int32_t turnPower = std::clamp(mapExponential(controller.get_analog(axis2)), (int32_t)-70, (int32_t)70);
+	left.move(-(drivePower +  turnPower));
+	right.move(-(drivePower -  turnPower));
+}
+
 void rightArcadeDrive(pros::v5::Controller& controller, const pros::v5::AbstractMotor& left, const pros::v5::AbstractMotor& right)
 {
 	left.move(controller.get_analog(pros::controller_analog_e_t::E_CONTROLLER_ANALOG_RIGHT_Y) + controller.get_analog(pros::controller_analog_e_t::E_CONTROLLER_ANALOG_RIGHT_X ));
@@ -44,11 +52,20 @@ void rightArcadeDrive(pros::v5::Controller& controller, const pros::v5::Abstract
 
 
 void opcontrol() 
-{
+{	
+	int swapped = 0;
 	 while(true)
 	 {
-		leftArcadeDrive(master,leftMotors,rightMotors, pros::controller_analog_e_t::E_CONTROLLER_ANALOG_LEFT_Y, 
+		if(swapped)
+		{
+		leftArcadeDriveSwapped(master, leftMotors, rightMotors, pros::controller_analog_e_t::E_CONTROLLER_ANALOG_LEFT_Y, 
 			pros::controller_analog_e_t::E_CONTROLLER_ANALOG_RIGHT_X);
+		}
+		else
+		{
+			leftArcadeDrive(master, leftMotors, rightMotors, pros::controller_analog_e_t::E_CONTROLLER_ANALOG_LEFT_Y, 
+			pros::controller_analog_e_t::E_CONTROLLER_ANALOG_RIGHT_X);
+		}
 		
 		if(master.get_digital(pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_R1))
 		{
@@ -63,6 +80,19 @@ void opcontrol()
 			intakeMotors.move(0);
 		}
 
+		if(master.get_digital(pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_L1))
+		{
+			unLoader.move(60);
+		}
+		else if(master.get_digital(pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_L2))
+		{
+			unLoader.move(-60);
+		}
+		else
+		{
+			unLoader.move(0);
+		}
+
 		if(master.get_digital_new_press(pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_UP))
 		{
 			lift.toggle();
@@ -72,10 +102,19 @@ void opcontrol()
 		{
 			hood.toggle();
 		}
+
 		if(master.get_digital_new_press(pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_A))
 		{
 			arm.toggle();
 			lift.extend();
+		}
+
+		if(master.get_digital_new_press(pros::controller_digital_e_t::E_CONTROLLER_DIGITAL_B))
+		{
+			if(swapped)
+			swapped = 0;
+			else
+			swapped = 1;
 		}
 		pros::delay(20);
 	 }
